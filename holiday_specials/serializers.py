a8bc54c982f928b4578d5347958d71604fddec7f
Bonnie Peters <bonniepeters@Bonnies-MacBook-Pro.local>
2019-11-14 07:26:24 -0700

    Added .create and .update to episode serializer

--- holiday_specials/serializers.py
+++ holiday_specials/serializers.py
@@ -19,3 +19,35 @@ class EpisodeSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Episode
         fields = ('id', 'show', 'Title', 'Released', 'Season', 'Episode', 'Runtime', 'Plot', 'Poster', 'imdbRating', 'imdbVotes', 'imdbID', 'imdbSeriesID', 'justWatchUrl', 'show_id',)
+        
+    def create(self, validated_data):
+        episode = Episode.objects.create(show=validated_data['show']['id'],
+            Title=validated_data['Title'], 
+            Released=validated_data['Released'], 
+            Season=validated_data['Season'],
+            Episode=validated_data['Episode'],
+            Runtime=validated_data['Runtime'],
+            Plot=validated_data['Plot'],
+            Poster=validated_data['Poster'],
+            imdbRating=validated_data['imdbRating'],
+            imdbVotes=validated_data['imdbVotes'],
+            imdbID=validated_data['imdbID'],
+            imdbSeriesID=validated_data['imdbSeriesID'],
+            justWatchUrl=validated_data['justWatchUrl'],)
+        return episode
+
+    def update(self, validated_data):
+        episode = Episode.objects.update(show=validated_data['show']['id'],
+            Title=validated_data['Title'], 
+            Released=validated_data['Released'], 
+            Season=validated_data['Season'],
+            Episode=validated_data['Episode'],
+            Runtime=validated_data['Runtime'],
+            Plot=validated_data['Plot'],
+            Poster=validated_data['Poster'],
+            imdbRating=validated_data['imdbRating'],
+            imdbVotes=validated_data['imdbVotes'],
+            imdbID=validated_data['imdbID'],
+            imdbSeriesID=validated_data['imdbSeriesID'],
+            justWatchUrl=validated_data['justWatchUrl'],)
+        return episode
