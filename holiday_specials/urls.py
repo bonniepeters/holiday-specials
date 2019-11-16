468aea8e7442b9fec6d9af5754240a933aa05258
Bonnie Peters <bonniepeters@Bonnies-MacBook-Pro.local>
2019-11-16 14:59:44 -0700

    Added create endpoint

--- holiday_specials/urls.py
+++ holiday_specials/urls.py
@@ -5,7 +5,7 @@ from rest_framework.routers import DefaultRouter
 urlpatterns = [
     path('shows', views.ShowList.as_view(), name='show_list'),
     path('show/add', views.ShowCreate.as_view(), name='show_create'),
-    path('episodes/add', views.ShowCreate.as_view(), name='episode_create'),
+    path('episodes/add', views.EpisodeCreate.as_view(), name='episode_create'),
     path('shows/<int:pk>', views.ShowDetail.as_view(), name='show_detail'),
     path('episodes', views.EpisodeList.as_view(), name='episode_list'),
     path('episodes/<int:pk>', views.EpisodeDetail.as_view(), name='episode_detail'),
