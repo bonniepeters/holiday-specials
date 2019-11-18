b2151a119ed97f584f6b6d5de31090d78cd73e9b
Bonnie Peters <bonniepeters@Bonnies-MacBook-Pro.local>
2019-11-17 19:20:04 -0700

    Trying

--- holiday_specials_project/urls.py
+++ holiday_specials_project/urls.py
@@ -13,15 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf.urls import include
-from django.urls import path
+from django.urls import path, include
 from django.contrib import admin
 from rest_framework_jwt.views import obtain_jwt_token
 
 urlpatterns = [
    path('admin/', admin.site.urls),
    path('', include('holiday_specials.urls')),
-   path('', include('accounts.urls')),
-   path('api-auth', include('rest_framework.urls', namespace = 'rest_framework')),
-    path('token-auth/', obtain_jwt_token),
+   path('token-auth/', obtain_jwt_token),
 ]
---
+++ holiday_specials_project/utils.py
@@ -0,0 +1,8 @@
+from holiday_specials.serializers import UserSerializer
+
+
+def my_jwt_response_handler(token, user=None, request=None):
+    return {
+        'token': token,
+        'user': UserSerializer(user, context={'request': request}).data
+    }
