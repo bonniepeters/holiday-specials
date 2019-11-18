f083a4ce2de8c5447463f35657885dae065e5b41
Bonnie Peters <bonniepeters@Bonnies-MacBook-Pro.local>
2019-11-17 19:43:48 -0700

    Undo

--- holiday_specials/views.py
+++ holiday_specials/views.py
@@ -1,37 +1,6 @@
 from rest_framework import generics
-from .serializers import ShowSerializer, EpisodeSerializer, UserSerializer, UserSerializerWithToken
+from .serializers import ShowSerializer, EpisodeSerializer
 from .models import Show, Episode
-from django.http import HttpResponseRedirect
-from django.contrib.auth.models import User
-from rest_framework import permissions, status
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-from rest_framework.views import APIView
-
-@api_view(['GET'])
-def current_user(request):
-    """
-    Determine the current user by their token, and return their data
-    """
-    
-    serializer = UserSerializer(request.user)
-    return Response(serializer.data)
-
-
-class UserList(APIView):
-    """
-    Create a new user. It's called 'UserList' because normally we'd have a get
-    method here too, for retrieving a list of all User objects.
-    """
-
-    permission_classes = (permissions.AllowAny,)
-
-    def post(self, request, format=None):
-        serializer = UserSerializerWithToken(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class ShowList(generics.ListAPIView):
     queryset = Show.objects.all()
