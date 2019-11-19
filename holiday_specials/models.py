af9fe0df047f029c35c99e188334d7c799eb5241
bonniepeters <bonnie.e.peters@gmail.com>
2019-11-18 19:18:45 -0700

    Minor adjustments to models

--- holiday_specials/models.py
+++ holiday_specials/models.py
@@ -2,13 +2,13 @@ from django.db import models
 
 class Show(models.Model):
     Title = models.CharField(max_length=60, default='')
-    Year = models.CharField(max_length=10, default='')
-    Genre = models.CharField(max_length=10, default='')
+    Year = models.CharField(max_length=60, default='')
+    Genre = models.CharField(max_length=60, default='')
     Plot = models.TextField(default='')
     Poster = models.TextField(default='')
     imdbRating = models.DecimalField(default=5, max_digits=2, decimal_places=1)
-    imdbVotes = models.CharField(max_length=30, default='')
-    imdbID = models.CharField(max_length=15, default='')
+    imdbVotes = models.CharField(max_length=60, default='')
+    imdbID = models.CharField(max_length=60, default='')
     totalSeasons = models.PositiveIntegerField(default=1)
     justWatchUrl = models.CharField(max_length=60, default='')
 
@@ -18,16 +18,16 @@ class Show(models.Model):
 class Episode(models.Model):
     show = models.ForeignKey(Show, on_delete=models.CASCADE, related_name='episodes')
     Title = models.CharField(max_length=100, default='')
-    Released = models.CharField(max_length=15, default='')
+    Released = models.CharField(max_length=60, default='')
     Season = models.PositiveIntegerField(default=1)
     Episode = models.PositiveIntegerField(default=10)
-    Runtime = models.CharField(max_length=15, default='')
+    Runtime = models.CharField(max_length=60, default='')
     Plot = models.TextField(default='')
     Poster = models.TextField(default='')
     imdbRating = models.DecimalField(default=5, max_digits=2, decimal_places=1)
-    imdbVotes = models.CharField(max_length=30, default='')
-    imdbID = models.CharField(max_length=15, default='')
-    imdbSeriesID = models.CharField(max_length=15, default='')
+    imdbVotes = models.CharField(max_length=60, default='')
+    imdbID = models.CharField(max_length=60, default='')
+    imdbSeriesID = models.CharField(max_length=60, default='')
     justWatchUrl = models.CharField(max_length=60, default='')
 
     def __str__(self):
